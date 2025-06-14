c813a12fe3a79205c0934200b3098f26984d324b
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-13 22:35:46 -0300

    refactor: update SideBar component to accept contacts, links, and pathName as props

--- src/containers/Navigation/SideBar.tsx
+++ src/containers/Navigation/SideBar.tsx
@@ -1,21 +1,17 @@
-"use client";
-
-import { NavItems } from "@/containers/NavItems/NavItems";
 import { UserProfile } from "@/components/UserProfile";
-import { contacts } from "@/data/contatos";
-import linksNavigation from "@/data/linksNavigation.json";
-import { usePathname } from "next/navigation";
+import { NavItems } from "@/containers/NavItems/NavItems";
 import { ContactLinks } from "../ContactLinks";
 
-export const SideBar = () => {
-  const pathName = usePathname();
+import * as I from "./Navigation.interface";
+
+export const SideBar = ({ contacts, links, pathName }: I.NavigationProps) => {
   return (
-    <article className="sticky top-0 hidden h-screen max-w-72 flex-col border-r border-gray-700 bg-gray-800 p-6 md:flex">
+    <aside className="sticky top-0 hidden h-screen max-w-72 flex-col border-r border-gray-700 bg-gray-800 p-6 md:flex">
       <UserProfile />
       <div className="mt-10 flex-1">
-        <NavItems links={linksNavigation} pathName={pathName} />
+        <NavItems links={links} pathName={pathName} />
       </div>
       <ContactLinks contacts={contacts} />
-    </article>
+    </aside>
   );
 };
