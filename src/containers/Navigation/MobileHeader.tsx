0ce8e29eda82d297d0c2049b6d11535132b83894
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-14 00:11:49 -0300

    fix: adjust padding the MobileHeader

--- src/containers/Navigation/MobileHeader.tsx
+++ src/containers/Navigation/MobileHeader.tsx
@@ -9,7 +9,7 @@ export const MobileHeader = ({
   pathName,
 }: I.NavigationProps) => {
   return (
-    <header className="flex h-16 w-full items-center justify-between border-b border-gray-700 px-2.5 md:hidden">
+    <header className="flex h-16 w-full items-center justify-between border-b border-gray-700 px-5 md:hidden">
       <UserProfile />
       <MobileMenu contacts={contacts} links={links} pathName={pathName} />
     </header>
