55ad77372b755cec6dcf5932ae2d57fa677df103
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-13 22:55:17 -0300

    fix: update language attribute in RootLayout to pt-BR for localization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={`${inter.variable} antialiased`}>
         <MobileHeader />
 
