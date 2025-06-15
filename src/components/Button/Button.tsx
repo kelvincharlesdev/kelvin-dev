6b686d5eb020f61a74ebadd1662f4621df05c696
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-14 21:22:59 -0300

    refactor: refactor component Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,9 @@
-import * as I from "./Button.inetrface";
+import * as I from "./Button.interface";
 
 export const Button = ({ children, ...props }: I.ButtonProps) => {
   return (
     <button
-      className="cursor-pointer rounded border border-2 border-blue-500 px-4 py-2 text-gray-100 shadow-md transition-colors hover:border-blue-600 hover:shadow-blue-600/50"
+      className="h-10 min-w-[100px] cursor-pointer rounded-lg border border-2 border-blue-600 px-4 py-2 text-sm text-gray-100 shadow-md transition-colors hover:border-blue-700 hover:shadow-blue-700/50"
       {...props}
     >
       {children}
