6f96febf80c7b8cad6c747664f99504279fa1e14
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-14 21:25:09 -0300

    feat: add content Hero in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,9 @@
+import { Hero } from "@/content/home";
+
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-900 text-gray-100 flex items-center justify-center flex-col p-6">
-      <h1>Home Page</h1>
-
-      {/* <div>
-        <h1 className="text-3xl font-bold">Título Principal</h1>
-        <h2 className="text-2xl font-semibold">Título Secundário</h2>
-        <h3 className="text-xl font-semibold">Título Menor</h3>
-        <p className="text-base">
-          Texto padrão para parágrafos e conteúdo geral.
-        </p>
-        <p className="text-sm text-textSecondary">
-          Texto secundário, legendas ou placeholders.
-        </p>
-        <small className="text-xs text-textSecondary">
-          Texto menor para detalhes.
-        </small>
-      </div>
-
-      <div className="bg-gray-800 border border-gray-700 rounded-2xl shadow-lg p-8 max-w-md w-full space-y-6">
-        <h1 className="text-3xl font-bold">Meu Portfólio</h1>
-        <p className="text-gray-200">
-          Bem-vindo! Aqui você encontra meus projetos como desenvolvedor
-          frontend.
-        </p>
-
-        <input
-          type="text"
-          placeholder="Seu nome"
-          className="w-full bg-transparent border border-gray-700 text-gray-100 placeholder-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-
-        <button className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-lg transition-colors">
-          Fale comigo
-        </button>
-
-        <a
-          href="#"
-          className="block text-blue-500 hover:text-blue-600 text-center font-medium"
-        >
-          Ver projetos →
-        </a>
-      </div> */}
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
