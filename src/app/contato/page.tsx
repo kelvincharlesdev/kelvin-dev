b7da9dc19690204e244d6edf0b269d2c9170b443
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-14 21:59:23 -0300

    feat: add feddback the pages in development

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,7 +1,8 @@
 export default function ContatoPage() {
   return (
-    <div>
-      <h1>Página Contato</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold">Página Contato</h1>
+      <p className="text-gray-400">EM DESENVOLVIMENTO...</p>
     </div>
   );
 }
