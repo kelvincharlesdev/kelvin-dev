70ac356740dcf6435a3e5a09418203d7eab377ca
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-17 22:31:54 -0300

    feat: add model and data the projects

---
+++ src/data/projects.ts
@@ -0,0 +1,34 @@
+import { Project } from "@/models/projects";
+
+export const projects: Project[] = [
+  {
+    id: 1,
+    title: "Dogs Next",
+    image: "/images/dogs-next.png",
+    link: "https://dogs-next-g4pa.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Finance AI",
+    image: "/images/finance-ai.png",
+    link: "https://finance-manager-q78m.vercel.app/login",
+  },
+  {
+    id: 3,
+    title: "Twitter Clone",
+    image: "/images/twitter-clone.png",
+    link: "https://twitter-clone-eosin-five.vercel.app/home",
+  },
+  {
+    id: 4,
+    title: "Poke Next",
+    image: "/images/poke-next.png",
+    link: "https://poke-next-chi-ecru.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "GitHub API",
+    image: "/images/github-api.png",
+    link: "https://projeto-mundo-real-fetch-api-github.vercel.app/",
+  },
+];
