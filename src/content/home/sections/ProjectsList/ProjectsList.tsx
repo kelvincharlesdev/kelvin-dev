9e31906c3de83a76a87e51afe3376ecb8a617a84
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-17 22:31:08 -0300

    feat: add content projectslist

---
+++ src/content/home/sections/ProjectsList/ProjectsList.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/Button";
+import { Title } from "@/components/Title";
+import { ProjectsList as ProjectListContainer } from "@/containers/ProjectsList";
+
+import { projects } from "@/data/projects";
+import Link from "next/link";
+
+export const ProjectsList = () => {
+  const recentProjects = projects.slice(0, 3);
+  return (
+    <div className="mt-6 flex flex-col md:mt-8 md:space-y-6">
+      <div className="flex items-center justify-between">
+        <Title as="h3">Projetos</Title>
+        <Link href="/projetos">
+          <Button>Ver todos os projetos</Button>
+        </Link>
+      </div>
+
+      <ProjectListContainer projects={recentProjects} />
+    </div>
+  );
+};
