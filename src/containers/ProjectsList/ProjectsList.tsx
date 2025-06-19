2cd067e54061985adf2684bab26f6ce1ba7984c1
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-18 22:54:30 -0300

    refactor: replace project list with CarouselProjects and ProjectsListDesktop components

--- src/containers/ProjectsList/ProjectsList.tsx
+++ src/containers/ProjectsList/ProjectsList.tsx
@@ -1,28 +1,12 @@
-import Link from "next/link";
-
-import Card from "@/components/Card";
+import { CarouselProjects, ProjectsListDesktop } from "./components";
 
 import * as I from "./ProjectsList.interface";
 
 export const ProjectsList = ({ projects }: I.ProjectsListProps) => {
   return (
-    // TODO: Colocar carousel na versão tablet e mobile
-    <ul className="grid gap-8 rounded-lg border-1 border-gray-700 p-4 lg:grid-cols-[repeat(auto-fit,minmax(280px,1fr))]">
-      {projects.map((project) => (
-        <li key={project.id} className="h-[280px]">
-          <Link href={project.link}>
-            <Card.Wrapper>
-              <Card.ImageBackground imageUrl={project.image} />
-              <Card.Footer>
-                <h2 className="text-sm font-bold text-gray-100">
-                  {project.title}
-                </h2>
-                <p className="text-sm font-bold text-gray-100">Saiba mais!</p>
-              </Card.Footer>
-            </Card.Wrapper>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <CarouselProjects projects={projects} />
+      <ProjectsListDesktop projects={projects} />
+    </>
   );
 };
---
+++ src/containers/ProjectsList/components/index.ts
@@ -0,0 +1,2 @@
+export * from "./CarouselProjects";
+export * from "./ProjectsListDesktop";
