164ca8c5d075163dc7bf2858d9d1668a8625d15b
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-21 00:42:31 -0300

    fix: change div for section in component StacksList

--- src/content/home/sections/StacksLink/StacksLink.tsx
+++ src/content/home/sections/StacksLink/StacksLink.tsx
@@ -5,9 +5,9 @@ import { stacks } from "@/data/stacks";
 
 export const StacksList = () => {
   return (
-    <div className="mt-8 flex flex-col space-y-6 pb-8">
+    <section className="mt-8 flex flex-col space-y-6 pb-8">
       <Title as="h3">Stacks</Title>
       <StacksListContainer stacks={stacks} />
-    </div>
+    </section>
   );
 };
