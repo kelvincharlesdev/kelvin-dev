8b752ac9c4d0c1e264ea7fc29be6e042d61f49a3
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-25 20:41:41 -0300

    feat: add content Hero in projects

---
+++ src/content/projects/sections/Hero/Hero.tsx
@@ -0,0 +1,23 @@
+import { Hero as HeroProjects } from "@/containers/Hero";
+import { Title } from "@/components/Title";
+import Link from "next/link";
+import { Button } from "@/components/Button";
+
+export const Hero = () => {
+  return (
+    <div>
+      <HeroProjects>
+        <Title>Projetos</Title>
+
+        <div className="mt-10 flex gap-4">
+          <Link href="/contato">
+            <Button>Contatos</Button>
+          </Link>
+          <Link href="#">
+            <Button>A decidir</Button>
+          </Link>
+        </div>
+      </HeroProjects>
+    </div>
+  );
+};
