b6713b07f0ac797de60dce35dfd9aed6fb26eb42
Kelvin Charles <kelvinc1291@hotmail.com>
2025-06-25 17:10:11 -0300

    feat: add StacksList component and update exports in sections

--- src/content/home/sections/index.ts
+++ src/content/home/sections/index.ts
@@ -1,3 +1,3 @@
 export * from "./Hero";
 export * from "./ProjectsList";
-export * from "./StacksLink";
+export * from "@/content/globals/StacksLink";
--- src/content/sobre/sections/index.ts
+++ src/content/sobre/sections/index.ts
@@ -1,3 +1,4 @@
 export * from "./Hero";
 export * from "./WhoIAm";
 export * from "./ProfessionalExperience";
+export * from "@/content/globals/StacksLink";
