1b72b4a97df5c493c4861c425b92ad2159e2fe2e
Kelvin Charles <kelvinc1291@hotmail.com>
2025-07-01 19:39:04 -0300

    fix: update build

--- src/app/curiosidades/page.tsx
+++ src/app/curiosidades/page.tsx
@@ -2,7 +2,7 @@ export default function CuriosidadesPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-2xl font-bold">Página Curiosidades</h1>
-      <p className="text-gray-400">EM DESENVOLVIMENTO...</p>
+      <p className="text-gray-400">EM DESENVOLVIMENTO....</p>
     </div>
   );
 }
