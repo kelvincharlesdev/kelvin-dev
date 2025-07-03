1cb418f90ef22379dd37d140abd5e4dffd2fd690
Kelvin Charles <kelvinc1291@hotmail.com>
2025-07-03 19:20:29 -0300

    refactor: alter interface StackCardProps to use IconType for icon property

--- src/models/stacks.ts
+++ src/models/stacks.ts
@@ -1,7 +1,9 @@
+import { IconType } from "react-icons";
+
 export interface StackCardProps {
   id?: number;
   title: string;
   label: string;
-  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  icon: IconType;
   link?: string;
 }
