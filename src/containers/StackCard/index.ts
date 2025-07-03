643e45175834a0ee1612ee8512bf67d1d2a9cefb
Kelvin Charles <kelvinc1291@hotmail.com>
2025-07-03 19:05:16 -0300

    refactor: remove StackCard component and update index to export StackCardElements

--- src/containers/StackCard/index.ts
+++ src/containers/StackCard/index.ts
@@ -1,1 +1,11 @@
-export * from "./StackCard";
+import { StackCardContainer } from "./StackCardContainer";
+import { Icon } from "./Icon";
+import { Info } from "./Info";
+
+const StackCardElements = {
+  StackCardContainer,
+  Icon,
+  Info,
+};
+
+export default StackCardElements;
