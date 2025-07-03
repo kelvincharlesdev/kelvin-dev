79e8c2adbd2843fec2964108e7bfc136f5ce5cf1
Kelvin Charles <kelvinc1291@hotmail.com>
2025-07-03 20:16:18 -0300

    feat: update page not found message to include 404 status code

--- src/app/projetos/projeto/not-found.tsx
+++ src/app/projetos/projeto/not-found.tsx
@@ -6,12 +6,12 @@ import { Title } from "@/components/Title";
 export default function NotFound() {
   return (
     <div className="flex min-h-[calc(100vh-33px)] flex-col items-center justify-center gap-4 p-8 text-center">
-      <Title as="h1">404 - Página não encontrada</Title>
+      <Title as="h1">Projeto não encontrado!</Title>
       <p className="text-gray-400">
-        A página que você está procurando não existe ou foi movida.
+        O projeto que você está tentando acessar não existe ou foi removido.
       </p>
       <Link href="/projetos">
-        <Button>Voltar ao início</Button>
+        <Button>Voltar para projetos</Button>
       </Link>
     </div>
   );
